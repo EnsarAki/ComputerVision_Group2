8742c910bd6e75fadbd34ce401739728121b1605
Nikola <nik9721@hotmail.com>
2023-02-21 11:55:26 +0100

    Added plotting of location in frame

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from scipy.io import loadmat
 from os.path import dirname, join as pjoin
+import numpy as np
+import math
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 import scipy.io as sio
 if __name__ == '__main__':
-    print("hello world")
     mat_fname = pjoin('vidf-cvpr', 'vidf1_33_000_frame_full.mat')
     framecount = loadmat(mat_fname)
     frame = framecount['frame']
-    print(frame[0, 0]['loc'])
+    frameloc = frame[0, 0]['loc']
 
     img = mpimg.imread('ucsdpeds/vidf/vidf1_33_000.y/vidf1_33_000_f001.png')
-    img[96, 54] = 1
-    imgplot = plt.imshow(img)
+    for x in frameloc[0][0]:
+        img[(math.floor(x[1])), math.floor(x[0])] = 1
+        print(x[1], x[0])
+
+    imgplot = plt.imshow(img, "gray")
     print(img.size)
     plt.show()
