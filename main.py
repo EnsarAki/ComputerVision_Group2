445a7f54be2fa0b17db05693332aa1ee4488976b
Nego24 <nik9721@hotmail.com>
2023-02-28 18:31:33 +0100

    Added loop through whole dataset. Further a density map is created and saved in a local folder. Functions for loading location data and image added.

--- main.py
+++ main.py
@@ -2,37 +2,49 @@ from scipy.io import loadmat
 from os.path import dirname, join as pjoin
 import numpy as np
 import math
-
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from scipy.stats import multivariate_normal
-from PIL import Image
 
-import scipy.io as sio
+
+def loadFrameLoc(vidNum, frameNum):
+    # Checking if video and frame number is lower than 1
+    if frameNum < 1 or vidNum < 1:
+        print("Video or frame number smaller than 1 does not exist.")
+    # Formatting video number for loading
+    vidNum = "{0:0=3d}".format(vidNum - 1)
+
+    # Loading video locations for the frame
+    mat_fname = pjoin('vidf-cvpr', f'vidf1_33_{vidNum}_frame_full.mat')
+    loadedFrame = loadmat(mat_fname)
+
+    return loadedFrame['frame'][0, (frameNum - 1)]['loc'][0][0]
+
+
+def loadImage(vidNum, frameNum):
+    # Checking if video and frame number is lower than 1
+    if frameNum < 1 or vidNum < 1:
+        print("Video or frame number smaller than 1 does not exist.")
+
+    # Formatting video number and frame number for loading
+    vidNum = "{0:0=3d}".format(vidNum - 1)
+    frameNum = "{0:0=3d}".format(frameNum)
+    return mpimg.imread(f'ucsdpeds/vidf/vidf1_33_{vidNum}.y/vidf1_33_{vidNum}_f{frameNum}.png')
+
+
 if __name__ == '__main__':
-    mat_fname = pjoin('vidf-cvpr', 'vidf1_33_000_frame_full.mat')
-    framecount = loadmat(mat_fname)
-    frame = framecount['frame']
-    frameloc = frame[0, 0]['loc']
-
-    img = mpimg.imread('ucsdpeds/vidf/vidf1_33_000.y/vidf1_33_000_f001.png')
-
     sigma = 5
-    cov_matrix = [[sigma, 0], [0, sigma]]
-    column_size = len(img[0])
-    row_size = len(img)
+    cov_matrix = np.array([[sigma, 0], [0, sigma]])
+    img_density = np.zeros((158, 238))
+    mean = np.zeros(2)
 
-    img_density = np.zeros((row_size, column_size))
+    for vidNum in range(1, 10):
+        for frameNum in range(1, 201):
+            img = loadImage(vidNum, frameNum)
+            location = loadFrameLoc(vidNum, frameNum)
+            for loc in location:
+                mean.put([0, 1], [math.floor(loc[1]), math.floor(loc[0])])
+                for i in range(158):
+                    for j in range(238):
+                        img_density[i, j] = img_density[i, j] + multivariate_normal.pdf([i, j], mean, cov_matrix)
+            mpimg.imsave(f'vidf-cvpr-density-map/vidf1_33_{vidNum}_f{frameNum}.png', img_density)
 
-    for n in frameloc[0][0]:
-        x = math.floor(n[1])
-        y = math.floor(n[0])
-        mean = [x, y]
-        for i in range(row_size):
-            for j in range(column_size):
-                img_density[i, j] = img_density[i, j] + multivariate_normal.pdf([i, j], mean, cov_matrix)
-
-    imgplot1 = plt.imshow(img, "gray")
-    plt.show()
-    imgplot2 = plt.imshow(img_density)
-    plt.show()
