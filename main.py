fc23d3e868dad4b0091e0b185d20a7ea361aa720
Nikolaj <nikolajsloth@hotmail.dk>
2023-02-28 15:11:42 +0100

    Gaussian pdf on input frame added

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import math
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from scipy.stats import multivariate_normal
+from PIL import Image
 
 import scipy.io as sio
 if __name__ == '__main__':
@@ -14,10 +16,23 @@ if __name__ == '__main__':
     frameloc = frame[0, 0]['loc']
 
     img = mpimg.imread('ucsdpeds/vidf/vidf1_33_000.y/vidf1_33_000_f001.png')
-    for x in frameloc[0][0]:
-        img[(math.floor(x[1])), math.floor(x[0])] = 1
-        print(x[1], x[0])
 
-    imgplot = plt.imshow(img, "gray")
-    print(img.size)
+    sigma = 5
+    cov_matrix = [[sigma, 0], [0, sigma]]
+    column_size = len(img[0])
+    row_size = len(img)
+
+    img_density = np.zeros((row_size, column_size))
+
+    for n in frameloc[0][0]:
+        x = math.floor(n[1])
+        y = math.floor(n[0])
+        mean = [x, y]
+        for i in range(row_size):
+            for j in range(column_size):
+                img_density[i, j] = img_density[i, j] + multivariate_normal.pdf([i, j], mean, cov_matrix)
+
+    imgplot1 = plt.imshow(img, "gray")
+    plt.show()
+    imgplot2 = plt.imshow(img_density)
     plt.show()
